137914c3e55b4971b9f9578c4391c0f9a094e26a
Stuart Green <stuart.green@cmuk.org>
2018-09-04 09:37:24 +0100

    fix split() deprecated and not available in PHP7 +

--- src/IdacMedia/IntelliSms/IntelliSms.php
+++ src/IdacMedia/IntelliSms/IntelliSms.php
@@ -1,37 +1,26 @@
 <?php
-
 namespace IdacMedia\IntelliSms;
-
 // IntelliSoftware IntelliSMS PHP Library
 // Release: v1.0
-
-
 class IntelliSms
 {
-
     //User Properties:
-
-
     /**
      * Set the Username and Password properties with your account details
      */
     public $Username = "";
     public $Password = "";
-
     /**
      * Maximum multipart messages that will be used
      */
     public $MaxConCatMsgs = 1;
-
     /**
      * Service URLs (change to https for secure connection)
      * NOTE: To use https you will need the OpenSSL extension module
      */
     public $PrimaryGateway = "http://www.intellisoftware.co.uk";
     public $BackupGateway = "http://www.intellisoftware2.co.uk";
-
     //User Methods:
-
     /**
      * Send regular text message
      */
@@ -45,17 +34,11 @@ class IntelliSms
             'text' => $text,
             'maxconcat' => $this->MaxConCatMsgs
         );
-
         $formData = http_build_query($formParams, '', '&');
-
         $response = $this->IntelliSMS_MakeHTTPRequest($formData);
-
         $sendStatusCollection = $this->IntelliSMS_ParseHTTPResponse($response, $to);
-
         return $sendStatusCollection;
     }
-
-
     //Send WAP Push message
     public function SendWapPushMessage($to, $text, $href, $from)
     {
@@ -68,17 +51,11 @@ class IntelliSms
             'text' => $text,
             'href' => $href
         );
-
         $formData = http_build_query($formParams, '', '&');
-
         $response = $this->IntelliSMS_MakeHTTPRequest($formData);
-
         $sendStatusCollection = $this->IntelliSMS_ParseHTTPResponse($response, $to);
-
         return $sendStatusCollection;
     }
-
-
     //Send Unicode message
     public function SendUnicodeMessageHex($to, $unicodehex, $from)
     {
@@ -91,17 +68,11 @@ class IntelliSms
             'hex' => $unicodehex,
             'maxconcat' => $this->MaxConCatMsgs
         );
-
         $formData = http_build_query($formParams, '', '&');
-
         $response = $this->IntelliSMS_MakeHTTPRequest($formData);
-
         $sendStatusCollection = $this->IntelliSMS_ParseHTTPResponse($response, $to);
-
         return $sendStatusCollection;
     }
-
-
     //Send Binary message
     public function SendBinaryMessage($to, $userdataheaderhex, $userdatahex, $from)
     {
@@ -114,17 +85,11 @@ class IntelliSms
             'udh' => $userdataheaderhex,
             'ud' => $userdatahex
         );
-
         $formData = http_build_query($formParams, '', '&');
-
         $response = $this->IntelliSMS_MakeHTTPRequest($formData);
-
         $sendStatusCollection = $this->IntelliSMS_ParseHTTPResponse($response, $to);
-
         return $sendStatusCollection;
     }
-
-
     //Send regular text message with UserContext
     public function SendMessageWithUserContext($to, $text, $usercontext)
     {
@@ -136,17 +101,11 @@ class IntelliSms
             'text' => $text,
             'maxconcat' => $this->MaxConCatMsgs
         );
-
         $formData = http_build_query($formParams, '', '&');
-
         $response = $this->IntelliSMS_MakeHTTPRequest($formData, $to);
-
         $sendStatusCollection = $this->IntelliSMS_ParseHTTPResponse($response, $to);
-
         return $sendStatusCollection;
     }
-
-
     //Get account balance
     public function GetBalance()
     {
@@ -154,25 +113,16 @@ class IntelliSms
             'username' => $this->Username,
             'password' => $this->Password
         );
-
         $formData = http_build_query($formParams, '', '&');
-
         $response = $this->IntelliSMS_MakeHTTPRequestToURL($formData, "/smsgateway/getbalance.aspx");
-
         $results = $this->IntelliSMS_ParseHTTPResponse_GetBalance($response);
-
         return $results;
     }
-
-
     //Implementation Functions:
-
-
     private function IntelliSMS_MakeHTTPRequest($data)
     {
         return $this->IntelliSMS_MakeHTTPRequestToURL($data, "/smsgateway/sendmsg.aspx");
     }
-
     private function IntelliSMS_MakeHTTPRequestToURL($data, $urlpath)
     {
         try {
@@ -186,11 +136,8 @@ class IntelliSms
                 throw $e;
             }
         }
-
         return $response;
     }
-
-
     private function IntelliSMS_MakeHTTPRequestUsingGateway($gatewayid, $data, $urlpath)
     {
         if ($gatewayid == 1) {
@@ -202,44 +149,33 @@ class IntelliSms
                 throw new \Exception("Gateway Id invalid $gatewayid");
             }
         }
-
         return $this->MakeHTTPFormPost($gatewayurl, $data, "Content-Type: application/x-www-form-urlencoded\r\n");
     }
-
-
     private function IntelliSMS_ParseHTTPResponse($response, $to)
     {
         $const_IdPrefix = "ID:";
         $const_ErrPrefix = "ERR:";
-
         $SendStatusCollection = array();
-
-        $msgresponses = split("\n", $response);
-
+        $msgresponses = explode("\n", $response);
         $idx = 0;
-
         foreach ($msgresponses as $msgresponse) {
             $msgresponse = trim($msgresponse);
-
             if (strlen($msgresponse) > 0) {
-                $msgresponseparts = split(",", $msgresponse);
-
+                $msgresponseparts =  explode(",", $msgresponse);
                 $msisdn = null;
                 $msgid = null;
                 $errorstatus = null;
-
                 if (count($msgresponseparts) >= 2) {
                     $msisdn = $msgresponseparts[0];
                     $msgresult = $msgresponseparts[1];
                 } else {
-                    if (count(split(",", $to)) == 1) {
+                    if (count(explode(",", $to)) == 1) {
                         $msisdn = $to;
                     } else {
                         $msisdn = "";
                     }
                     $msgresult = $msgresponseparts[0];
                 }
-
                 if (strncmp($msgresult, $const_IdPrefix, strlen($const_IdPrefix)) == 0) {
                     $msgid = substr($msgresult, strlen($const_IdPrefix));
                     $errorstatus = "OK";
@@ -249,26 +185,19 @@ class IntelliSms
                         $errorstatus = substr($msgresult, strlen($const_ErrPrefix));
                     }
                 }
-
                 $SendStatusCollection[$idx]["To"] = $msisdn;
                 $SendStatusCollection[$idx]["MessageId"] = $msgid;
                 $SendStatusCollection[$idx]["Result"] = $errorstatus;
-
                 $idx++;
             }
         }
-
         return $SendStatusCollection;
     }
-
-
     private function IntelliSMS_ParseHTTPResponse_GetBalance($response)
     {
         $const_BalancePrefix = "BALANCE:";
         $const_ErrPrefix = "ERR:";
-
         $results = array();
-
         if (strncmp($response, $const_BalancePrefix, strlen($const_BalancePrefix)) == 0) {
             $results["Balance"] = substr($response, strlen($const_BalancePrefix));
             $results["ErrorStatus"] = "OK";
@@ -278,52 +207,37 @@ class IntelliSms
                 $results["ErrorStatus"] = substr($response, strlen($const_ErrPrefix));
             }
         }
-
         return $results;
     }
-
-
     private function MakeHTTPFormPost($url, $data, $optional_headers = null)
     {
         $params = array('http' => array('method' => 'POST', 'content' => $data));
-
         if ($optional_headers !== null) {
             $params['http']['header'] = $optional_headers;
         }
-
         $ctx = stream_context_create($params);
-
         $fp = @fopen($url, 'rb', false, $ctx);
         if (!$fp) {
             throw new \Exception("Problem making HTTP request $url, $php_errormsg");
         }
-
         $response = @stream_get_contents($fp);
         if ($response === false) {
             throw new \Exception("Problem reading HTTP Response $url, $php_errormsg");
         }
-
         return $response;
     }
-
 }
-
-
 //Unicode helper functions
-
 function utf8toucs2hex($utf8)
 {
     $utf8_hex = bin2hex($utf8);
     return utf8hextoucs2hex($utf8_hex);
 }
-
 function utf8hextoucs2hex($str)
 {
     $ucs2 = "";
-
     for ($i = 0; $i < strlen($str); $i += 2) {
         $char1hex = $str[$i] . $str[$i + 1];
-
         $char1dec = hexdec($char1hex);
         if ($char1dec < 128) {
             $results = $char1hex;
@@ -348,14 +262,10 @@ function utf8hextoucs2hex($str)
                 }
             }
         }
-
         while (strlen($results) < 4) {
             $results = '0' . $results;
         }
-
         $ucs2 .= $results;
     }
-
     return $ucs2;
 }
-
